35dec43106ab06008154d1184040a748cd769b2b
Leonid <123672169+LBondarchuk@users.noreply.github.com>
2024-02-07 13:11:31 +0200

    e

--- src/utils/analitics.js
+++ src/utils/analitics.js
@@ -3,5 +3,5 @@ import { analytics } from '@/firebase';
 import { logEvent } from 'firebase/analytics';
 
 export const logButtonClickEvent = (name) => {
-  logEvent(analytics, name);
+  logEvent(analytics, name + '1');
 };
