00887313b9bac2cb738c3991c5559a2c9c64b520
Leonid <123672169+LBondarchuk@users.noreply.github.com>
2024-02-07 15:21:16 +0200

    edits

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "933abd5d2b79f576"; }
+/******/ 		__webpack_require__.h = function() { return "d4e334e84a3d5ba3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
